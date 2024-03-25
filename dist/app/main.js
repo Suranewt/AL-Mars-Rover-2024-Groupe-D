ce04166d4fad71c58e0eaabc78d4e9213b164371
Jean-Kelly Seguin <jean-kelly.seguin@outlook.fr>
2024-03-25 11:44:04 +0100

    tuto socket pour envoie de msg

---
+++ dist/app/main.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const RoverBuilder_1 = require("../tests/utilities/RoverBuilder");
+let ROVER = RoverBuilder_1.RoverBuilder.default();
+ROVER = ROVER.avancer();
+ROVER = ROVER.tournerDroite();
+ROVER = ROVER.reculer();
+//# sourceMappingURL=main.js.map
---
+++ src/server.ts
@@ -0,0 +1,30 @@
+import * as express from "express";
+import { Server as SocketIOServer, Socket } from "socket.io";
+import * as path from "path";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const server = app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
+
+const io = new SocketIOServer(server);
+
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: "src/client" });
+});
+
+io.on("connection", (socket: Socket) => {
+  console.log("A user connected");
+  
+  socket.on("message", (message: string) => {
+    console.log(message);
+
+    socket.emit("message", message);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
+});
