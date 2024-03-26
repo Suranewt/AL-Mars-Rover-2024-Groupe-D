0635e7c9d4f33c4b8f4d0cd8de36450673bdb432
john1340a <jean-kelly.seguin@outlook.fr>
2024-03-26 09:10:11 +0100

    remove dist directory

--- dist/app/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const RoverBuilder_1 = require("../tests/utilities/RoverBuilder");
-let ROVER = RoverBuilder_1.RoverBuilder.default();
-ROVER = ROVER.avancer();
-ROVER = ROVER.tournerDroite();
-ROVER = ROVER.reculer();
-//# sourceMappingURL=main.js.map
