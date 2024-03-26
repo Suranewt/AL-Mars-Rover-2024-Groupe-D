7a4420834ab6bae2a60d7f8e1cd22c258e5e8acd
Achref Awaissi <Achrefawaissi@gmail.com>
2024-03-26 14:51:36 -0700

    Ajout de cas test serialize

--- src/tests/parsing.test.ts
+++ src/tests/parsing.test.ts
@@ -23,4 +23,22 @@ describe('Rover Parser', () => {
 
         expect(roverTesté).toEqual(roverTémoin);
     })
-});
+    //teste des cas serialize (rover renvoie les cordonnées)
+    test.each([
+        [0, 0, Orientation.Nord, "0,0,N"],
+        [0, 0, Orientation.Sud, "0,0,S"],
+        [1, 0, Orientation.Nord, "1,0,N"],
+    ])
+    ("A un Rover de latitude %s, de longitude %s et orienté %s, correspond une string %s",
+        (latitude, longitude, orientation, strAttendu) => {
+            const planèteCommune = new PlaneteInfinie();
+            const rover = new Rover(
+                new Point(latitude, longitude),
+                orientation,
+                planèteCommune);
+
+            const representation = RoverInterpreter.serialize(rover);
+            expect(representation).toEqual(strAttendu);
+        })
+
+})
