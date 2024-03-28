1a3a72bfcf817bb559ca047f3b23b4a69e22eae0
Suranewt <saranyudaorueang@gmail.com>
2024-03-28 16:48:46 +0100

    Update rover-runner.ts

--- src/rover-runner.ts
+++ src/rover-runner.ts
@@ -2,7 +2,7 @@ import { RoverListener } from './rover-listener/RoverListener';
 import { activeRover } from './conf/mission.conf';
 import { WebSocketImplementation } from './network/web-socket/WebSocketImplementation';
 
-let rover = activeRover;
+const rover = activeRover;
 
 const iface = new WebSocketImplementation(rover);
 new RoverListener(rover, iface);
